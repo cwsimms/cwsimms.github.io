160d159f4cdde9d22f4bc5e600f9a09270174b63
Christopher Simms <cw_simms@sbcglobal.net>
2023-04-18 13:27:49 -0700

    + updateOverlay now works.

--- HomePage/homepagescript.js
+++ HomePage/homepagescript.js
@@ -12,19 +12,15 @@ function toggleOverlay(clickedElement) {
 
 function updateOverlay(inElement) {
     if (inElement.className == "project-title") {
-        const tileChildren = [];
-        tileChildren.push(inElement.childNodes);
-        //Set the title text of the overlay to that of the project tile clicked
-        //apparently childNodes does not return an array but a NodeList?
-        document.getElementById("project-overlay-title").innerText = tileChildren.find(element => element.className == "project-title").innerText;
-        let tileImg = tileChildren.find(element => element.className == "project-img");
-
-        //I know this should probably be a function, but I'm only using this once so far. If I use it again I'll make it a function.
-        let imgAttributes = [tileImg.getAttribute("src"), tileImg.getAttribute("alt")];
-        let overlayImg = document.getElementById("project-overlay-img");
-        overlayImg.setAttribute("src", imgAttributes[0]);
-        overlayImg.setAttribute("alt", imgAttributes[1]);
-
-        document.getElementById("project-overlay-desc").innerText = tileChildren.find(element => element.className == "project-desc-long").innerText;
+       document.getElementById("project-overlay-title").innerText = inElement.innerText;
+       const tile = inElement.parentElement.parentElement;
+       for (const child of tile.children) {
+        if (child.className == "project-image") {
+            document.getElementById("project-overlay-image").setAttribute("src", child.getAttribute("src"));
+        }
+        else if (child.className == "project-desc-long") {
+            document.getElementById("project-overlay-desc").innerText = child.innerText;
+        }
+       }
     }
 }
