985e8d8d22d269c9155c812cd2480589cfb7e8ec
Christopher Simms <cw_simms@sbcglobal.net>
2023-04-25 16:19:47 -0700

    + Added FPSWizard image (though may want a higher-res version
    ~ Moved script tag to the bottom of body
    
    ~ In updateOverlay, we now get tile-image by getting the child of tile-image-wrapper, rather than trying (and failing) to get tile-image through tile.children directly

--- HomePage/homepagescript.js
+++ HomePage/homepagescript.js
@@ -1,5 +1,4 @@
 function toggleOverlay(clickedElement) {
-    console.log("toggleOverlay called.");
     const element = document.getElementById("project-overlay-bg");
     if (clickedElement.className == "project-title") {
         element.style.display = "block";
@@ -15,8 +14,9 @@ function updateOverlay(inElement) {
        document.getElementById("project-overlay-title").innerText = inElement.innerText;
        const tile = inElement.parentElement.parentElement;
        for (const child of tile.children) {
-        if (child.className == "project-image") {
-            document.getElementById("project-overlay-image").setAttribute("src", child.getAttribute("src"));
+        if (child.className == "project-image-wrapper") {
+            let tileImage = child.children[0];
+            document.getElementById("project-overlay-image").setAttribute("src", tileImage.getAttribute("src"));
         }
         else if (child.className == "project-desc-long") {
             document.getElementById("project-overlay-desc").innerText = child.innerText;
